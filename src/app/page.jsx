4bf03dd80b88a88d1ee4bfdf5a6d2ac65e33b61b
Hunarship <itteamhunar@gmail.com>
2024-03-31 13:32:09 +0530

    First

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,12 @@
 import styles from "./homepage.module.css";
+import Link from "next/link";
+import './globals.css';
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <Link href="/"><h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi repudiandae nemo quisquam dignissimos. Impedit quia magnam quae soluta ullam facilis reprehenderit fuga consequuntur, ab quaerat dicta, ipsam labore suscipit sit?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, magnam fugiat voluptas aliquid molestiae vel, veritatis minus dolorem tempore quaerat laborum nam deserunt explicabo cupiditate autem enim sint quidem soluta.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit dolore sunt omnis laudantium quas veritatis, officiis eum illo id vero reiciendis inventore sint, eaque fugit dicta molestias placeat dignissimos expedita.
+    Lorem, ipsum dolor sit amet </h1>consectetur adipisicing elit. Incidunt quis accusamus quibusdam ea aliquam non eveniet illo dolores tenetur odio. Nemo quas mollitia ipsa beatae veniam odit voluptates saepe repellendus!</Link></div>;
 }
+
